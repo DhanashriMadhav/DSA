d5bcda6611143bc17eb0f2c3a932295a9893b0ff
DhanashriMadhav <79438293+DhanashriMadhav@users.noreply.github.com>
2021-04-13 09:30:44 +0530

    Create bst.py

---
+++ binarysearchtree/bst.py
@@ -0,0 +1,31 @@
+def main():
+    nums = [12, 6, 18, 19, 21, 11, 3, 5, 4, 24, 18]
+    bst = BSTNode()
+    for num in nums:
+        bst.insert(num)
+    print("preorder:")
+    print(bst.preorder([]))
+    print("#")
+
+    print("postorder:")
+    print(bst.postorder([]))
+    print("#")
+
+    print("inorder:")
+    print(bst.inorder([]))
+    print("#")
+
+    nums = [2, 6, 20]
+    print("deleting " + str(nums))
+    for num in nums:
+        bst.delete(num)
+    print("#")
+
+    print("4 exists:")
+    print(bst.exists(4))
+    print("2 exists:")
+    print(bst.exists(2))
+    print("12 exists:")
+    print(bst.exists(12))
+    print("18 exists:")
+    print(bst.exists(18))
